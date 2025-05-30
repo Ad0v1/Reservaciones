edeaefae13738296e34db3935c4dcf94852115cc
Ad0v1 <2201020079@undc.edu.pe>
2025-05-29 21:00:21 -0500

    v1.1.

--- logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-    session_start(); // Inicia la sesión
-    session_destroy(); // Destruye todas las variables de sesión header("Location: login.php"); // Redirige al usuario al login exit();
-?>
