9b1cdb18fb94ba316742e424aca5b4e56d3bd8c5
Ad0v1 <2201020079@undc.edu.pe>
2025-06-12 23:07:53 -0500

    Actualización frontend

--- frontend/assets/js/calendario.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        events: 'reservas.php' // Cargamos las reservas desde PHP
-    });
-    calendar.render();
-});
